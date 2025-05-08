cc74e7abb7fc4d006ac87294490bd92cbf1f961b
COrangeblade <13561058462@163.com>
2025-05-08 19:37:44 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 document.getElementById("login-btn").onclick = () => {
   const clientId = "Ov23liupPam9trKuoXLw";
-  const redirectUri = "https://corangeblade.github.io";
+  const redirectUri = "https://corangeblade.github.io/callback.html";
   window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user`;
 };
